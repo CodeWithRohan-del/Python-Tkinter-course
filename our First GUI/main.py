7a0532271d930f31e0b77c8e16f92bbcd728dae7
CodeWithRohan-del <dubeyshivansh8100@gmail.com>
2025-09-25 21:36:37 +0530

    Create main.py
    Add our first program file

---
+++ our First GUI/main.py
@@ -0,0 +1,14 @@
+
+#---------------------------------Create a first Gui in python -------------------------------
+
+#go to Follow this steps
+
+#------------------STEP 1 IMPORT TKINTER AS TK------------------------
+import tkinter as tk
+
+#--------------------STEP 2 CREATE A TK() OBJECT ------------------
+root = tk.Tk()
+
+#-------------------STEP 3 RUN THE EVENT LOOP ---------------
+root.mainloop()
+#----------FIRST GUI WINDOW IS READY --------------------
