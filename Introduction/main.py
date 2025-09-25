e7e421b79b08d7242398667af69cbd4a69756c9d
CodeWithRohan-del <dubeyshivansh8100@gmail.com>
2025-09-25 10:03:03 +0530

    Create main.py
    Add introduction file

---
+++ Introduction/main.py
@@ -0,0 +1,21 @@
+
+
+#---------------------------------Welcome to New Tkinter Course------------------------------
+
+#introduction to tkinter - Tkinter is a python bulit in module way to create a GUI in python #best features and pro way to  create GUI
+
+#-----------Application GUI Example -------------------
+
+#Calculetor
+#notepad
+#code Eidtor
+#all othes 
+
+#----------code 
+# This code present on Github Account link on Descrition
+
+#bey
+
+
+
+
