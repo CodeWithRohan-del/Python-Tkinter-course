77f8e39b61256edbe8c032b073884050024a2e7d
CodeWithRohan-del <dubeyshivansh8100@gmail.com>
2025-10-16 20:33:14 +0530

    add Exercise 3

---
+++ Exercise 3/main.py
@@ -0,0 +1,11 @@
+
+
+#------------------Exercise -3 ----------------
+
+#create a Form in Tkinter with best title
+# Input the name , age Email ,phone,pin code 
+# And save the user imforamtion on txt file
+# use only tkinter concpt you Learn with me
+
+# Get shoutout form me - best program create get winner
+#-----------------solve Exercise -3 ------------------
