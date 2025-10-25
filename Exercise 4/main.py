820eacdba70b4733ff7d838054321b629b34ab53
CodeWithRohan-del <dubeyshivansh8100@gmail.com>
2025-10-25 12:00:28 +0530

    Exercise 4 file

---
+++ Exercise 4/main.py
@@ -0,0 +1,15 @@
+
+#-----------------Exercise 4 -------------------
+
+# 1.Create a Tkinter window with Background color
+# 2. add Menubar and add menu in main menubar
+ # - file menu
+ #Edit menu
+    #help menu
+# 3 .back the buttons in Menubar
+# 4.Create a text area with backgrouond 
+
+#-----------solve Exercise ------------
+# --------get shoutout--Like and subcribe ------------
+
+# ------------code my Github Account updated  check link-----------
